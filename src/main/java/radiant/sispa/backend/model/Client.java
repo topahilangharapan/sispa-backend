0a0bf59ec0d1d639c58365a589efb79f02a2308c
Dinda Kirana Khairunnisa <dinda.kirana21@ui.ac.id>
2025-03-09 10:21:09 +0700

    (PBI-8, PBI-10, PBI-13): PUT Edit Client Information, GET View All Client, GET View Detail Client

---
+++ src/main/java/radiant/sispa/backend/model/Client.java
@@ -0,0 +1,71 @@
+package radiant.sispa.backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    private String id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "contact", nullable = false)
+    private String contact;
+
+    @NotNull
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @NotNull
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @NotNull
+    @Column(name = "industry", nullable = false)
+    private String industry;
+
+    @NotNull
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @NotNull
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @NotNull
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "deleted_at")
+    private Date deletedAt;
+
+}
