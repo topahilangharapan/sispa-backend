9aaea2f88aaa457ee08ecff705e5ea3c02a93c15
dindakiranaa <dindakirana104@gmail.com>
2025-03-21 04:36:09 +0700

    feat:fix update detail client and vendor

--- src/main/java/radiant/sispa/backend/restservice/ClientRestServiceImpl.java
+++ src/main/java/radiant/sispa/backend/restservice/ClientRestServiceImpl.java
@@ -91,7 +91,7 @@ public class ClientRestServiceImpl implements ClientRestService {
     }
 
     @Override
-    public ClientResponseDTO updateClient(String id, UpdateClientRequestRestDTO clientDTO) {
+    public ClientResponseDTO updateClient(String id, UpdateClientRequestRestDTO clientDTO, String username) {
         Client client = clientDb.findById(id).orElse(null);
         if (client == null || client.getDeletedAt() != null){
             return null;
@@ -103,6 +103,8 @@ public class ClientRestServiceImpl implements ClientRestService {
         client.setEmail(clientDTO.getEmail());
         client.setIndustry(clientDTO.getIndustry());
         client.setDescription(clientDTO.getDescription());
+
+        client.setUpdatedBy(username);
 
         Client updatedClient = clientDb.save(client);
         return clientToClientResponseDTO(updatedClient);
