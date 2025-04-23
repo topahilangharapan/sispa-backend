e561fbd0e88f2bbd5818026a3e3748d7afbf331b
Dinda Kirana Khairunnisa <dinda.kirana21@ui.ac.id>
2025-04-23 14:20:27 +0700

    Fix Update Item and Detail Item

--- src/main/java/radiant/sispa/backend/restdto/response/ItemResponseDTO.java
+++ src/main/java/radiant/sispa/backend/restdto/response/ItemResponseDTO.java
@@ -1,18 +1,6 @@
 package radiant.sispa.backend.restdto.response;
+import lombok.*;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-import radiant.sispa.backend.model.Category;
-import radiant.sispa.backend.model.ItemStatus;
-import radiant.sispa.backend.model.PurchaseOrderItem;
-
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -25,4 +13,5 @@ public class ItemResponseDTO {
     private String description;
     private String category;
     private String status;
+
 }
