7e12bb21a0352a46b3e985eb16fe10f48c03704a
topahilangharapan <musthofa.joko@ui.ac.id>
2025-05-18 20:11:43 +0700

    feat: id for expense and income

--- src/main/java/radiant/sispa/backend/repository/IncomeDb.java
+++ src/main/java/radiant/sispa/backend/repository/IncomeDb.java
@@ -1,11 +1,18 @@
 package radiant.sispa.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import radiant.sispa.backend.model.Income;
 import radiant.sispa.backend.model.Transaction;
 
+import java.time.Instant;
+
 @Repository
 public interface IncomeDb extends JpaRepository<Income, Long> {
+
+    @Query("SELECT COUNT(i) FROM Income i WHERE i.createdAt >= :startOfDay AND i.createdAt < :endOfDay")
+    long countIncomeToday(@Param("startOfDay") Instant startOfDay, @Param("endOfDay") Instant endOfDay);
 
 }
